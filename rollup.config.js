78abb7b03de4ae8a5a93fe18560f610408297d97
Wesley Luyten <me@wesleyluyten.com>
2019-12-15 22:10:06 -0500

    Move app to public folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,93 +1,100 @@
 import resolve from 'rollup-plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import {
+    terser
+} from 'rollup-plugin-terser';
 import bundleSize from 'rollup-plugin-size';
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
 
 const production = !process.env.ROLLUP_WATCH;
 
 const terserPlugin = terser({
-  sourcemap: true,
-  warnings: true,
-  compress: {
-    passes: 2
-  },
-  mangle: {
-    properties: {
-      regex: /^_/
+    sourcemap: true,
+    warnings: true,
+    compress: {
+        passes: 2
+    },
+    mangle: {
+        properties: {
+            regex: /^_/
+        }
     }
-  }
 });
 
 const config = {
-	input: 'src/lite-vimeo-embed.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'ltv',
-		file: 'dist/lite-vimeo-embed.js'
-	},
-	plugins: [
-		bundleSize(),
-		resolve(),
-        postcss({
-            extensions: [ '.css' ]
-        }),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terserPlugin
-	],
-	watch: {
-		clearScreen: false
-	}
-};
-
-export default [
-  {
-    ...config,
+    input: 'src/lite-vimeo-embed.js',
     output: {
-      ...config.output,
-      file: 'module/lite-vimeo-embed.js',
-      format: 'es'
-    }
-  },
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: 'dist/lite-vimeo-embed.js',
-      format: 'umd'
+        sourcemap: true,
+        format: 'iife',
+        name: 'ltv',
+        file: 'dist/lite-vimeo-embed.js'
     },
     plugins: [
-      ...config.plugins,
-      babel(),
+        bundleSize(),
+        resolve(),
+        postcss({
+            extensions: ['.css']
+        }),
 
-			// In dev mode, call `npm run start` once
-			// the bundle has been generated
-			!production && serve(),
+        // If we're building for production (npm run build
+        // instead of npm run dev), minify
+        production && terserPlugin
+    ],
+    watch: {
+        clearScreen: false
+    }
+};
 
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			!production && livereload('./')
+export default [{
+    ...config,
+    output: {
+        ...config.output,
+        file: 'module/lite-vimeo-embed.js',
+        format: 'es'
+    },
+    plugins: [
+        ...config.plugins,
+
+        copy({
+            targets: [{ src: 'dist', dest: 'public' }]
+        }),
     ]
-  },
-];
+}, {
+    ...config,
+    output: {
+        ...config.output,
+        file: 'dist/lite-vimeo-embed.js',
+        format: 'umd'
+    },
+    plugins: [
+        ...config.plugins,
+        babel(),
+
+        // In dev mode, call `npm run start` once
+        // the bundle has been generated
+        !production && serve(),
+
+        // Watch the `public` directory and refresh the
+        // browser on changes when not in production
+        !production && livereload('./')
+    ]
+}, ];
 
 function serve() {
-	let started = false;
+    let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+    return {
+        writeBundle() {
+            if (!started) {
+                started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+                require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+                    stdio: ['ignore', 'inherit', 'inherit'],
+                    shell: true
+                });
+            }
+        }
+    };
 }
